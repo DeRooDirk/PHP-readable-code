b5fea574a067df231e80adebe654448f35ecfa29
Zoo Wie <de-roo.dirk@hotmail.com>
2021-01-12 13:52:43 +0100

    Update in.php
    update on the vars in startfnction

--- in.php
+++ in.php
@@ -1,28 +1,26 @@
 <?php
 
 function getPizza ($pizzaType,$customer){
-	 getAdress($customer);
-	 getPrice($pizzaType);
-	
+	$Customer = ucfirst($customer);
+    $Adress = getAdress($customer);
+    $Price = getPrice($pizzaType);	
 
-	 echo 'create new ordrer .<br>' ;
-	 echo " a  $pizzaType pizza should be send to $customer .<br>";
-	 echo " The adress :  .<br>" ;
-	 
-
+	echo 'Making  a  new order..!!!.<br>';
+    echo "A  delicious{$pizzaType} pizza should be sent to {$Customer} <br>";
+    echo "The adress: {$Adress}<br>";
+    echo "The bill is €{$Price}<br>";
+    echo "Order finished.<br><br>";
 }
-
 function getAdress($customer){
 	switch ($customer){
 		case 'Koen' :
 			return 'a yacht in Antwerp';
 		case 'Manuele':
 			return 'Somewhere in Belgium';
-		case 'Students' : 
+		case 'Students of Verou 1.26' : 
 			return 'BeCode Office';
 		default :
-		return 'unknown Error wing said it ';
-
+		 	return 'unknown Error wing said it ';
 	}
 }
 function getPrice ($pizzaType){
@@ -39,11 +37,10 @@ function getPrice ($pizzaType){
 		return 'On its Way to you !!!';
 	}
 }
-
 function order (){
-getPizza('calzone','koen');
-getPizza('marguerita','manuele');
-getPizza('golden','students ');
+getPizza('calzone','Koen');
+getPizza('marguerita','Manuele');
+getPizza('golden','Students ');
 };
 order ();
 
